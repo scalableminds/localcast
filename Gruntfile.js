559b90d320ef64e31c44babb4504821c8d84185e
Tom Herold <tom.herold@scalableminds.com>
2014-09-03 15:14:36 +0200

    updated grunt file to build standalone mac app

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,146 +4,147 @@ module.exports = function (grunt) {
   'use strict';
 
   // load all grunt tasks
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  var matchdep = require('matchdep')
+  var wrench = require('wrench');
+  var _ = require('lodash');
+  var fs = require('fs');
+  var path = require('path');
+
+  matchdep.filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
   // configurable paths
   var config = {
     app: 'app',
     build: 'build',
-    dist: 'dist',
-    tmp: 'tmp',
-    resources: 'resources'
+    dist: 'dist'
   };
 
   grunt.initConfig({
-  config: config,
-  clean: {
-    dist: {
-    files: [{
-      dot: true,
-      src: [
-      '<%= config.dist %>/*',
-      '<%= config.tmp %>/*',
-      '<%= config.build %>/*'
-      ]
-    }]
-    }
-  },
-  jshint: {
-    options: {
-    jshintrc: '.jshintrc'
+    config: config,
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+          '<%= config.build %>/*',
+          '<%= config.dist %>/releases/*'
+          ]
+        }]
+      }
     },
-    files: '<%= config.build %>/js/*.js'
-  },
-  copy: {
-    copyAssets: {
-      files: [{
-        expand: true,
-        cwd: '<%= config.app %>',
-        dest: '<%= config.build %>',
-        src: ['**', '!**/*.less', '!**/*.coffee']
-      }]
-    }
-  },
-  watch: {
-    options: {
-    livereload: true
+    jshint: {
+      options: {
+      jshintrc: '.jshintrc'
+      },
+      files: '<%= config.build %>/js/*.js'
+    },
+    copy: {
+      copyAssets: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>',
+          dest: '<%= config.build %>',
+          src: ['**', '!**/*.less', '!**/*.coffee']
+        }]
+      },
+      modules: {
+        files: [{
+          expand: true,
+          cwd: 'node_modules',
+          dest: '<%= config.build %>',
+          src: ['**/*']
+        }]
+      }
+    },
+    watch: {
+      options: {
+      livereload: true
+      },
+      coffee: {
+        files: '<%= config.app %>/js/**/*.coffee',
+        tasks: ["coffee:compile"]
+      },
+      less: {
+        files: '<%= config.app %>/css/**/*.less',
+        tasks: ["less"]
+      },
+      html: {
+        files:  '<%= config.app %>/**/*.html',
+        tasks: ["copy:copyAssets"]
+      },
+      images: {
+        files:  '<%= config.app %>/img/*',
+        tasks: ["copy:copyAssets"]
+      }
     },
     coffee: {
-      files: '<%= config.app %>/js/**/*.coffee',
-      tasks: ["coffee:compile"]
+      compile: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/js',
+          src: ['**/*.coffee'],
+          dest: '<%= config.build %>/js',
+          ext: ".js"
+        }]
+      }
     },
     less: {
-      files: '<%= config.app %>/css/**/*.less',
-      tasks: ["less"]
+      sources : {
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/css',
+          src: ['**/*.less'],
+          dest: '<%= config.build %>/css',
+          ext: ".css"
+        }]
+      }
     },
-    html: {
-      files:  '<%= config.app %>/**/*.html',
-      tasks: ["copy:copyAssets"]
+    connect: {
+      server: {
+      options: {
+        port: 8000,
+        livereload: true,
+        base: '<%= config.build %>'
+      }
+      }
     },
-    images: {
-      files:  '<%= config.app %>/img/*',
-      tasks: ["copy:copyAssets"]
-    }
-  },
-  coffee: {
-    compile: {
-      files: [{
-        expand: true,
-        cwd: '<%= config.app %>/js',
-        src: ['**/*.coffee'],
-        dest: '<%= config.build %>/js',
-        ext: ".js"
-      }]
-    }
-  },
-  less: {
-    sources : {
-      files: [{
-      expand: true,
-      cwd: '<%= config.app %>/css',
-      src: ['**/*.less'],
-      dest: '<%= config.build %>/css',
-      ext: ".css"
-      }]
-    }
-  },
-  connect: {
-    server: {
-    options: {
-      port: 8000,
-      livereload: true,
-      base: '<%= config.build %>'
-    }
-    }
-  },
-  shell: {
-    bower: {
-      command: "bower install"
+    shell: {
+      bower: {
+        command: "bower install"
+      }
     },
-    patch: {
-      command: './setup'
-    }
-  },
-  nodewebkit: {
-    options: {
-        version: '0.8.6', // make sure to change ./setup when touching this
-        macIcns: '<%= config.app %>/img/icon/osx.icns', //OSX only
-        winIco: '<%= config.app %>/img/icon/windows.ico', //Windows only
-        cacheDir: '<%= config.dist %>/cache',
-        buildDir: '<%= config.dist %>/releases',
-        platforms: [] // OS will be set by 'dist' task
+    nodewebkit: {
+      options: {
+          version: '0.8.6', // make sure to change ./setup when touching this
+          macIcns: '<%= config.app %>/img/icon/osx.icns', //OSX only
+          winIco: '<%= config.app %>/img/icon/windows.ico', //Windows only
+          cacheDir: '<%= config.dist %>/cache',
+          buildDir: '<%= config.dist %>/releases',
+          platforms: [] // OS will be set by 'dist' task
+      },
+      src: ['<%= config.build %>/**/*']
     },
-    src: ['<%= config.build %>/**/*']
-  },
+    packageNodeWebkit: {
+      options: {
+        dest: '<%= config.build %>'
+      }
+    }
   });
 
-  grunt.registerTask('dist-linux', [
-  'jshint',
-  'clean:dist',
-  'build',
-  ], function() {
+  grunt.registerTask('dist-linux', function() {
     grunt.config.set('nodewebkit.options.platforms', ['linux32', 'linux64'])
-    grunt.task.run("nodewebkit")
+    grunt.task.run("packageNodeWebkit")
   });
 
-  grunt.registerTask('dist-win', [
-  'clean:dist',
-  'build',
-  ], function() {
+  grunt.registerTask('dist-win', function() {
     grunt.config.set("nodewebkit.options.platforms", ['win'])
-    grunt.task.run("nodewebkit")
+    grunt.task.run("packageNodeWebkit")
   });
 
-  grunt.registerTask('dist-mac', [
-  'clean:dist',
-  'build',
-  'shell:patch'
-  ], function() {
+  grunt.registerTask('dist-mac', function() {
     grunt.config.set("nodewebkit.options.platforms", ['osx'])
-    grunt.task.run("nodewebkit")
-  }
-  );
+    grunt.task.run("packageNodeWebkit")
+  });
 
   grunt.registerTask('lint', [
   'jshint'
@@ -170,5 +171,32 @@ module.exports = function (grunt) {
 
   grunt.registerTask("default", ["build", "connect", "watch"])
   grunt.registerTask("build", ["shell:bower", "coffee:compile", "less:sources", "copy:copyAssets"])
+
+  grunt.registerTask("packageNodeWebkit", "Package project as standalone nodewebkit app", function(){
+    var options = this.options();
+
+    // Build coffeescript / less
+    grunt.task.run("clean:dist");
+    grunt.task.run("build");
+
+    // Copy node modlues app to compiled JS
+    wrench.mkdirSyncRecursive(path.join(options.dest, 'node_modules'));
+
+    var devDependancies = matchdep.filterDev('*');
+    var modulesDirectories = fs.readdirSync("node_modules");
+    _.difference(modulesDirectories, devDependancies).forEach(
+      function(directory) {
+        wrench.copyDirSyncRecursive(path.join('node_modules', directory), path.join(options.dest, 'node_modules', directory));
+      }
+    )
+
+    // Package app for specified platforms
+    grunt.task.run("nodewebkit");
+
+    // Remove copy of node_modules directory
+    // TODO Wait for the above "nodewebkit" task to finish before deleting the dir
+    //wrench.rmdirSyncRecursive(path.join(options.dest, 'node_modules'));
+
+  });
 
 }
